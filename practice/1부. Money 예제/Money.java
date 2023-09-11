6478685bb2cfa11cd73161325a7d746e0f15e34e
seungtaekhong <hsk4991149@naver.com>
2023-09-12 00:35:08 +0900

    feat: Money example final code

---
+++ practice/1부. Money 예제/Money.java
@@ -0,0 +1,45 @@
+public class Money implements Expression {
+    protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money Franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    public Expression times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+
+    public Money reduce(Bank bank, String to) {
+        int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        return new Money(amount / rate, to);
+    }
+
+    String currency() {
+        return currency;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && currency().equals(money.currency());
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
+    }
+}
